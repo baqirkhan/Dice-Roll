16d3dd98dabd3d59df787f38f0cbd5084e7ce12c
baqir <baqirkhan00@gmail.com>
2022-04-15 18:12:26 +0530

    Add over for how to play, first cut

--- dist/js/app.js
+++ dist/js/app.js
@@ -23,8 +23,23 @@ const playerTwoCurrent = document.getElementById("player-2-current");
 
 const dice = document.querySelector(".dice-image");
 const winnerOverlay = document.querySelector(".winner");
+const overlay = document.querySelector(".overlay");
+const howTo = document.getElementById("how-to");
+const newGame = document.getElementById("new-game");
 const rollDice = document.getElementById("roll-dice");
 const holdScore = document.getElementById("hold-score");
+
+const startNewGame = () => {
+  players["1"] = {
+    current: 0,
+    overall: 0,
+  };
+  players["2"] = {
+    current: 0,
+    overall: 0,
+  };
+  playerActive = 1;
+};
 
 const checkIfWon = (diceValue) => {
   if (diceValue === 1) {
@@ -40,7 +55,6 @@ const checkIfWon = (diceValue) => {
 };
 
 const updateValues = () => {
-  console.log(players);
   playerOneOverall.textContent = players["1"].overall.toString();
   playerTwoOverall.textContent = players["2"].overall.toString();
   playerOneCurrent.textContent = players["1"].current.toString();
@@ -50,6 +64,17 @@ const updateValues = () => {
 winnerOverlay.addEventListener("click", () =>
   winnerOverlay.classList.add("winner-hidden")
 );
+
+overlay.addEventListener("click", () => overlay.classList.add("hidden"));
+
+howTo.addEventListener("click", () => {
+  overlay.classList.remove("hidden");
+});
+
+newGame.addEventListener("click", () => {
+  startNewGame();
+  updateValues();
+});
 
 holdScore.addEventListener("click", () => {
   if (playerActive === 1) {
@@ -70,6 +95,6 @@ rollDice.addEventListener("click", () => {
   dice.src = `images/dice-${diceValue}.png`;
   players[playerActive].current += diceValue;
   players[playerActive].overall += diceValue;
-  checkIfWon(diceValue);
+  if (checkIfWon(diceValue)) startNewGame();
   updateValues();
 });
